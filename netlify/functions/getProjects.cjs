6510eaf2c91d54e0f54a837c74e0c2767768eb01
Lucas Zhang <lhzhang@live.unc.edu>
2021-10-05 22:55:41 -0400

    Made functions a little bit more proper

--- netlify/functions/getProjects.cjs
+++ netlify/functions/getProjects.cjs
@@ -1,9 +1,5 @@
 const { Client } = require("@notionhq/client");
 
-const notion = new Client({
-  auth: process.env.NOTION_TOKEN,
-});
-
 function fromNotionObject(notionPage) {
   return {
     id: notionPage.id,
@@ -12,16 +8,26 @@ function fromNotionObject(notionPage) {
 }
 
 exports.handler = async () => {
-  const { NOTION_PROJECTS_DATABASE } = process.env;
-
-  const projects = await notion.databases.query({
-    database_id: NOTION_PROJECTS_DATABASE,
+  const { NOTION_TOKEN, NOTION_PROJECTS_DATABASE } = process.env;
+  const notion = new Client({
+    auth: NOTION_TOKEN,
   });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: projects.results.map(fromNotionObject)
-    }),
-  };
+  try {
+    const projects = await notion.databases.query({
+      database_id: NOTION_PROJECTS_DATABASE,
+    });
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: projects.results.map(fromNotionObject)
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch projects from notion' })
+    }
+  }
 };
--- netlify/functions/getWork.cjs
+++ netlify/functions/getWork.cjs
@@ -1,38 +1,46 @@
 const { Client } = require("@notionhq/client");
 
-const notion = new Client({
-  auth: process.env.NOTION_TOKEN,
-});
-
 function fromNotionObject(notionPage) {
   return {
     id: notionPage.id,
-    ...notionPage.properties
-  }
+    ...notionPage.properties,
+  };
 }
 
 exports.handler = async () => {
-  const { NOTION_WORK_DATABASE, NOTION_WORK_START, NOTION_WORK_END } =
+  const { NOTION_TOKEN, NOTION_WORK_DATABASE, NOTION_WORK_START, NOTION_WORK_END } =
     process.env;
-
-  const work = await notion.databases.query({
-    database_id: NOTION_WORK_DATABASE,
-    sorts: [
-      {
-        property: NOTION_WORK_START,
-        direction: "descending",
-      },
-      {
-        property: NOTION_WORK_END,
-        direction: "descending",
-      },
-    ],
+  const notion = new Client({
+    auth: NOTION_TOKEN,
   });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: work.results.map(fromNotionObject)
-    }),
-  };
+  try {
+    const work = await notion.databases.query({
+      database_id: NOTION_WORK_DATABASE,
+      sorts: [
+        {
+          property: NOTION_WORK_START,
+          direction: "descending",
+        },
+        {
+          property: NOTION_WORK_END,
+          direction: "descending",
+        },
+      ],
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: work.results.map(fromNotionObject),
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch work from notion' })
+    }
+  }
+
+  
 };
