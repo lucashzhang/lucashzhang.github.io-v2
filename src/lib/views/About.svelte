34eb2792dd47df2e177e9abab208a285327783c5
Lucas Zhang <lhzhang@live.unc.edu>
2021-10-30 23:54:23 -0400

    More about

--- src/lib/views/About.svelte
+++ src/lib/views/About.svelte
@@ -5,15 +5,28 @@
 <section id="about">
   <img src="/Me.jpg" alt="Me" loading="lazy" />
   <Window color="primary">
-    <h1>About Me</h1>
-    <hr />
-    <p>
-      Hello! My name is Lucas Zhang. I'm a creative and analytical software
-      developer who also happens to love making things look pretty.
-    </p>
-    <div class="Button-Group">
-      <a class="Button" href="https://linkedin.com/in/lucashzhang" target="_blank">Linkedin</a>
-      <a class="Button" href="https://drive.google.com/file/d/1kYoxdLcPnO5h7ehvS6RkL8_2aIfyihdu/view?usp=sharing" target="_blank">Resume</a>
+    <div class="About-Content">
+      <h1>About Me</h1>
+      <hr />
+      <p>
+        Hello! My name is Lucas Zhang. I'm a creative and analytical software
+        developer who also happens to love making things look pretty. I've been
+        interning, teaching, taking classes, and building all sorts of projects,
+        hoping to gain more practical experience in my field as well as explore
+        any new ones.
+      </p>
+      <div class="Button-Group">
+        <a
+          class="Button"
+          href="https://linkedin.com/in/lucashzhang"
+          target="_blank">Linkedin</a
+        >
+        <a
+          class="Button"
+          href="https://drive.google.com/file/d/1kYoxdLcPnO5h7ehvS6RkL8_2aIfyihdu/view?usp=sharing"
+          target="_blank">Resume</a
+        >
+      </div>
     </div>
   </Window>
 </section>
@@ -24,6 +37,7 @@
     flex-direction: row;
     justify-content: center;
     padding-top: 3rem;
+    height: 100%;
   }
 
   img {
@@ -35,9 +49,17 @@
     width: 48rem;
   }
 
+  .About-Content {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
   .Button-Group {
-    margin-top: 2rem;
+    margin-top: auto;
+    margin-bottom: 0.5rem;
     display: grid;
+
     grid-template-columns: 1fr 1fr;
     gap: 1rem;
   }
