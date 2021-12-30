26c32eb289cff2d5261d94662f7110a94b674e89
Lucas Zhang <lhzhang@live.unc.edu>
2021-12-29 22:18:54 -0500

    Gradient logo background

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,17 +11,3 @@
   <slot />
 </main>
 <Footer />
-<img id="website3d" src="/Website3d.png" alt="">
-
-<style>
-  #website3d {
-    position: fixed;
-    right: -160px;
-    top: 30vh;
-    z-index: -1;
-    height: 80vh;
-    transform: rotateZ(10deg);
-    opacity: 0.7;
-    filter: blur(2px);
-  }
-</style>
