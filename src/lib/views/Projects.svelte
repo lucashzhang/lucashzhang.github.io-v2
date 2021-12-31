e788d652fc7af8a1383daa41aa115ee48b14cdf9
Lucas Zhang <lhzhang@live.unc.edu>
2021-12-31 11:34:57 -0500

    Original stroke widths

--- src/lib/views/Projects.svelte
+++ src/lib/views/Projects.svelte
@@ -27,7 +27,7 @@
         <path
           stroke-linecap="round"
           stroke-linejoin="round"
-          stroke-width="1.5"
+          stroke-width="2"
           d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
         />
       </svg>
